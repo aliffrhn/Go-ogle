4f45f23af4858cd9e6b972c75e377844df885c22
aliffrhn <alif.farhanw@gmail.com>
2019-07-11 17:04:59 +0700

    add comment multiline

--- komentar.go
+++ komentar.go
@@ -4,5 +4,7 @@ import "fmt"
 
 func main(){
 	fmt.Println("Hello World") //Comment
-	fmt.Println("Baris Komen")
+	// fmt.Println("Baris Komen")
+	/*fmt.Println("Comment MultiLine")
+	fmt.Println("LALALALAL")*/
 }
