b94b6f36ffcc8e989fd1845fff327a78e3422747
aliffrhn <alif.farhanw@gmail.com>
2019-08-08 11:47:53 +0700

    test all operator

--- operator.go
+++ operator.go
@@ -1,3 +1,15 @@
 package main
 
 import "fmt"
+
+func main(){
+	// initial var
+	var x = 10
+	var z = 5
+
+	fmt.Println("Penjumlahan", x + z)
+	fmt.Println("Pengurangan", x - z)
+	fmt.Println("Pembagian", x / z)
+	fmt.Println("Perkalian", x * z)
+	fmt.Println("Modulus", x % z)
+}
