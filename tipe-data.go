184ec093d0e301747e1dcd41cde16584a0ae6101
aliffrhn <alif.farhanw@gmail.com>
2019-08-08 11:25:14 +0700

    check negatif

--- tipe-data.go
+++ tipe-data.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main(){
+	// Positif
 	var number_positif8 uint8 =  255 //0-255
 	var number_positif16 uint16 = 65534 //0-65535
 	var number_positif32 uint32 = 4000000 //0-4294967295
@@ -12,4 +13,8 @@ func main(){
 	fmt.Println(number_positif16)
 	fmt.Println(number_positif32)
 	fmt.Println(number_positif64)
+
+	// Negatif
+	var number_negatif int64 = -99
+	fmt.Println(number_negatif)
 }
