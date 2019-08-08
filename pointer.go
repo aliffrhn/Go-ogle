3adabcf760ff6f6b843a74ced6b608ddde103e02
aliffrhn <alif.farhanw@gmail.com>
2019-08-08 14:08:56 +0700

    add new example with string pointer

--- pointer.go
+++ pointer.go
@@ -3,9 +3,18 @@ package main
 import "fmt"
 
 func main(){
+
 	var number int =  10
 	var address_number *int = &number
 
 	fmt.Println("Number:",number)
 	fmt.Println("Address Number:",address_number)
+
+	// 
+
+	var tas string = "Hash"
+	var address_tas *string = &tas
+	
+	fmt.Println("Tas:",tas)
+	fmt.Println("Address Tas",address_tas)
 }
