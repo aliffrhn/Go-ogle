da73276b7ce7536175061d199f0c3b5501bd4886
aliffrhn <alif.farhanw@gmail.com>
2019-08-08 12:53:08 +0700

    create map.go

--- map.go
+++ map.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main(){
+
+	var harga_makanan = map[string]int{"ayam_goreng":20000, "sate_lalat":15000}\
 	
+	// Show  
+	fmt.Println(harga_makanan["ayam_goreng"])
 }
