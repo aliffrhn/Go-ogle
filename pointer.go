fb33fe26584011b72f2967fd9dbd5aa070ce9c88
aliffrhn <alif.farhanw@gmail.com>
2019-08-08 14:07:02 +0700

    change minor

--- pointer.go
+++ pointer.go
@@ -6,6 +6,6 @@ func main(){
 	var number int =  10
 	var address_number *int = &number
 
-	fmt.Println(number)
-	fmt.Println(address_number)
+	fmt.Println("Number:",number)
+	fmt.Println("Address Number:",address_number)
 }
