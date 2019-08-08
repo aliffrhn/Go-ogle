2c5d14e46d86db26bb66a2d5c403d3371dbeb275
aliffrhn <alif.farhanw@gmail.com>
2019-08-08 12:38:14 +0700

    try array structure, length, get fruit, before and after

--- array.go
+++ array.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main(){
+
+	// Define Fruits With Arr
+	var fruits = [4]string{"Apple", "Manggo", "Pienapple", "Cocoa"}
 	
+	// Get Length with len()
+	fmt.Println("Length = ", len(fruits))
+
+	// Get Fruits
+	fmt.Println("Result Before Update = ", fruits)
+
+	// Update
+	fruits[1] = "Ayam"
+
+	fmt.Println("Result After Update = ", fruits)
 }
