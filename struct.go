3adc75f1a2466a85980de68f4e1943c4aa147e13
aliffrhn <alif.farhanw@gmail.com>
2019-08-08 14:18:42 +0700

    add new x2 pelajar 2

--- struct.go
+++ struct.go
@@ -5,6 +5,7 @@ import "fmt"
 func main(){
 	
 	var x1 pelajar
+
 	x1.nama = "John"
 	x1.kelas = 12
 	x1.umur = 20
@@ -12,6 +13,16 @@ func main(){
 	fmt.Println("Nama:", x1.nama)
 	fmt.Println("Kelas:", x1.kelas)
 	fmt.Println("Umur:", x1.umur)
+
+	var x2 pelajar
+
+	x2.nama = "John2"
+	x2.kelas = 12
+	x2.umur = 20
+
+	fmt.Println("Nama:", x2.nama)
+	fmt.Println("Kelas:", x2.kelas)
+	fmt.Println("umur:", x2.umur)
 }
 
 type pelajar struct{
