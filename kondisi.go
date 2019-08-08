cb5f70cec7ef58f0c41e771b4a5487c24a17a56a
aliffrhn <alif.farhanw@gmail.com>
2019-08-08 11:52:58 +0700

    create nilai kondisi result

--- kondisi.go
+++ kondisi.go
@@ -4,4 +4,14 @@ import "fmt"
 
 func main(){
 	
+	var nilai = 9
+	if nilai == 10{
+		fmt.Println("Lulus Dengan Baik")
+	}else if nilai >= 7{
+		fmt.Println("Lulus")
+	}else if nilai == 6{
+		fmt.Println("Belum Lulus")
+	}else {
+		fmt.Println("Belajar Lagi")
+	}
 }
