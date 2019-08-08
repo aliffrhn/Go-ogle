96fb423d705d7f6e8344db23f105b820648a7559
aliffrhn <alif.farhanw@gmail.com>
2019-08-08 12:54:44 +0700

    add length arr

--- map.go
+++ map.go
@@ -4,8 +4,11 @@ import "fmt"
 
 func main(){
 
-	var harga_makanan = map[string]int{"ayam_goreng":20000, "sate_lalat":15000}\
+	var harga_makanan = map[string]int{"ayam_goreng":20000, "sate_lalat":15000}
 	
 	// Show  
 	fmt.Println(harga_makanan["ayam_goreng"])
+
+	// Show Len
+	fmt.Println(len(harga_makanan))
 }
