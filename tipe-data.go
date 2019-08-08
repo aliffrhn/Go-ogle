52b81942a930c251f7d43e9a312ed9c9141fe8ff
aliffrhn <alif.farhanw@gmail.com>
2019-08-08 11:23:19 +0700

    tes all uint

--- tipe-data.go
+++ tipe-data.go
@@ -5,5 +5,11 @@ import "fmt"
 func main(){
 	var number_positif8 uint8 =  255 //0-255
 	var number_positif16 uint16 = 65534 //0-65535
+	var number_positif32 uint32 = 4000000 //0-4294967295
+	var number_positif64 uint64 = 400000000000 //0-
+
 	fmt.Println(number_positif8)
+	fmt.Println(number_positif16)
+	fmt.Println(number_positif32)
+	fmt.Println(number_positif64)
 }
