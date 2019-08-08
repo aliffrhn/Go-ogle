48364bfc86bc026a9ee05075f23e022fd1d30bd9
aliffrhn <alif.farhanw@gmail.com>
2019-08-08 11:58:00 +0700

    test switch case

--- kondisi.go
+++ kondisi.go
@@ -17,4 +17,16 @@ func main(){
 	}else {
 		fmt.Println("Belajar Lagi")
 	}
+
+	//
+
+	// Condtion
+	switch nilai{
+	case 10:
+		fmt.Println("Nilai 10")
+	case 9:
+		fmt.Println("Nilai 9")
+	case 8:
+		fmt.Println("Nilai 8")
+	}
 }
