4e3bc44c2b144f7ac98b9c5842a98423a42f67a2
aliffrhn <alif.farhanw@gmail.com>
2019-08-08 13:14:01 +0700

    add tampilkan pesan4 to return 2 variable

--- fungsi.go
+++ fungsi.go
@@ -6,7 +6,8 @@ func main(){
 	
 	tampilkan_pesan1()
 	fmt.Println(tampilkan_pesan2())
-	fmt.Println(tampilkan_pesan3(2,5))
+	fmt.Println(tampilkan_pesan3(10,2))
+	fmt.Println(tampilkan_pesan4(10,2))
 }
 
 func tampilkan_pesan1(){
@@ -20,4 +21,10 @@ func tampilkan_pesan2()string{
 func tampilkan_pesan3(x int, y int)int{
 	var z = x * y
 	return z
+}
+
+func tampilkan_pesan4(x int, y int)(int,int){
+	var z = x * y
+	var a = x / y
+	return z,a
 }
