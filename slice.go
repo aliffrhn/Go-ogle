d41b7831ee0247acfe7d4f4c3f89122e9e23b5e2
aliffrhn <alif.farhanw@gmail.com>
2019-08-08 12:43:34 +0700

    create check length, and call fruit index

--- slice.go
+++ slice.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main(){
+
+	// Define Array withour length
+	var fruits = []string{"apple", "manggo", "melon", "pienaple"}
 	
+	// Check length fruits
+	fmt.Println(len(fruits))
+
+	// Call fruits in index 1
+	fmt.Println(fruits[0])
 }
