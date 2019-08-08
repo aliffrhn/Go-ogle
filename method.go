56ea3e591820774ea6d4c9244f609074758a2da8
aliffrhn <alif.farhanw@gmail.com>
2019-08-08 14:27:37 +0700

    create method to call name

--- method.go
+++ method.go
@@ -4,4 +4,19 @@ import "fmt"
 
 func main(){
 	
+	var s1 pelajar
+	var s2 = pelajar{"yosep", 18}
+	s1.nama = "aldo"
+	s1.umur = 19
+	s1.namasaya()
+	s2.namasaya()
+}
+
+type pelajar struct{
+	nama string
+	umur int
+}
+
+func (s pelajar) namasaya(){
+	fmt.Println("Nama saya adalah", s.nama)
 }
