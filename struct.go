8eace67eabff3e6044c21f2bd53ee6c6f74d6f2b
aliffrhn <alif.farhanw@gmail.com>
2019-08-08 14:13:52 +0700

    fix minor err

--- struct.go
+++ struct.go
@@ -3,11 +3,19 @@ package main
 import "fmt"
 
 func main(){
+	
+	var x1 pelajar
+	x1.nama = "John"
+	x1.kelas = 12
+	x1.umur = 20
 
+	fmt.Println("Nama:", x1.nama)
+	fmt.Println("Kelas:", x1.kelas)
+	fmt.Println("Umur:", x1.umur)
 }
 
 type pelajar struct{
-	nama string,
-	kelas int,
+	nama string
+	kelas int
 	umur int
 }
