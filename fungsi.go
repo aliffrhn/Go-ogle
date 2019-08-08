28fa3fa8082676b1c202bb15df49357e9a4e3d44
aliffrhn <alif.farhanw@gmail.com>
2019-08-08 13:02:35 +0700

    add tampilkan pesan 2 func

--- fungsi.go
+++ fungsi.go
@@ -4,10 +4,15 @@ import "fmt"
 
 func main(){
 	
-	fmt.Println(tampilkan_pesan())
+	tampilkan_pesan1()
+	tampilkan_pesan2()
+	// tampilkan_pesan3()
 }
 
-func tampilkan_pesan()string{
-	
-	return "Pesan Berhasil Diterima"
+func tampilkan_pesan1(){
+	fmt.Println("Pesan Diterima 1")
+}
+
+func tampilkan_pesan2(){
+	return "Pesan Diterima 2"
 }
