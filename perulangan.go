3c1a637bc0bc22e545820932dfc75c65c0cb1f28
aliffrhn <alif.farhanw@gmail.com>
2019-08-08 12:06:47 +0700

    add while perulangan

--- perulangan.go
+++ perulangan.go
@@ -3,7 +3,18 @@ package main
 import "fmt"
 
 func main(){
+
+	// With For
 	for i:=1;i<=10;i++{
 		fmt.Println("Angka", i)
+	}
+
+	// Define Nilai
+	var nilai = 1
+
+	// With While
+	for nilai<=10{
+		fmt.Println("Nilai", nilai)
+		nilai++
 	}
 }
